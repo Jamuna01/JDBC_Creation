1fd9c26f393f9692f58efc6471dcb86fcb923aa7
jamuna Adhikari <adhikarijam@gmail.com>
2021-02-02 18:16:41 -0800

    Using select (Query)

---
+++ demonstration/SelectDemo.java
@@ -0,0 +1,37 @@
+package jdbc.demonstration;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class SelectDemo 
+{
+public static final String SQL = "select * from user_tbl";
+	
+	
+	
+	public static void main(String[] args) throws ClassNotFoundException
+	{
+		
+		try {
+
+			PreparedStatement ps = DbUtil.getConnection().prepareStatement(SQL);
+			
+			//ps.setInt(1, 1);
+			
+			ResultSet rs = ps.executeQuery();
+			while(rs.next())
+			{
+				System.out.println("Id is: " + rs.getInt(1));
+				System.out.println("name is: " + rs.getString(2));
+				System.out.println("password is: " + rs.getString(3));
+				System.out.println("=====================");
+			}
+			
+		} catch (SQLException e) {
+			
+			e.printStackTrace();
+		}
+		
+		}
+}
