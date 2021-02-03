1f47b8f671e6414d142009a1519d61010f0916b9
jamuna Adhikari <adhikarijam@gmail.com>
2021-02-02 20:41:11 -0800

    Printing selected data

---
+++ demonstration/SelectDemo2.java
@@ -0,0 +1,37 @@
+package jdbc.demonstration;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class SelectDemo2 
+{
+public static final String SQL = "select * from user_tbl where id=?";
+	
+	
+	
+	public static void main(String[] args) throws ClassNotFoundException
+	{
+		
+		try {
+
+			PreparedStatement ps = DbUtil.getConnection().prepareStatement(SQL);
+			
+			ps.setInt(1, 2);
+			
+			ResultSet rs = ps.executeQuery();
+			while(rs.next())
+			{
+				System.out.println("Id is: " + rs.getInt("id"));
+				System.out.println("Username is: " + rs.getString("user_name"));
+				System.out.println("Password is: " + rs.getString("password"));
+				System.out.println("=====================");
+			}
+			
+		} catch (SQLException e) {
+			
+			e.printStackTrace();
+		}
+		
+	}
+}
