29d37e18cb0b1891ba8310d144df63a5f07c303a
jamuna Adhikari <adhikarijam@gmail.com>
2021-02-02 17:10:32 -0800

    Inserted Data into table

--- demonstration/InsertDemo.java
+++ demonstration/InsertDemo.java
@@ -1,5 +1,34 @@
 package jdbc.demonstration;
 
-public class InsertDemo {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 
+public class InsertDemo 
+{
+
+	
+	public static final String SQL = "insert into user_tbl(user_name, password)values('Ram', 'aa')";
+	
+	
+	//insert into user_tbl(user_name, password)values('Ram', 'aa')"
+	//insert into user_tbl(user_name, password)values('Jamuna', 'kkk')"
+	//insert into user_tbl(user_name, password)values('Dev', ''jfdljf'')"
+	public static void main(String[] args) throws ClassNotFoundException
+	{
+		
+		try {
+
+			Connection con = DbUtil.getConnection();
+			Statement st = con.createStatement();
+			
+			st.executeUpdate(SQL);
+			System.out.printf("Inserted Data!");
+		} catch (SQLException e) {
+			
+			e.printStackTrace();
+		}
+		
+		}
 }
